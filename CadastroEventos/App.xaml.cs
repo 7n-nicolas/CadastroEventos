78f6c6f23835aa60414832e56e1ff5165230db92
7N.Nicolas <7n.nicolas@gmail.com>
2026-05-27 18:19:03 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroEventos/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CadastroEventos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new AppShell());
+            window.Width = 400;
+            window.Height = 650;
+            return window;
+        }
+    }
+}
